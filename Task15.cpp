8baacb6a562152b1f4702c62c2555b73229c484b
Alzhan01 <57897830+Alzhan01@users.noreply.github.com>
2019-11-19 07:49:02 +0600

    Update Task15.cpp

--- Task15.cpp
+++ Task15.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 using namespace std;
 int main(){
-	double a,b,c;
-	cin>>a>>b>>c;
-	cout<<a*2<<"  "<<b/3<<"  "<<pow(c,2)<<endl<<a*2+b/3+pow(c,2);
+	double a;
+	cin>>a;
+	cout<<pow(a,2)<<endl<<pow(a,3);
     return 0;
 }
