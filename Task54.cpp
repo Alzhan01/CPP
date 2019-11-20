8652a7fb4858bec7d1f08fc7d5f75bdc019ccf8e
Alzhan01 <57897830+Alzhan01@users.noreply.github.com>
2019-11-20 22:01:18 +0600

    Add files via upload

---
+++ Task54.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double a,b,c;
+	int count;
+	cin>>a>>b>>c;
+	if(a>0){
+	count++;
+	}
+	if(b>0){
+	count++;
+	}
+	if(c>0){
+	count++;
+	}
+	cout<<count;
+	return 0;
+}
