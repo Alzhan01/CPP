0ed693433f116edbdfd700332d33e6e26b32d657
Alzhan01 <57897830+Alzhan01@users.noreply.github.com>
2019-11-21 07:48:01 +0600

    Add files via upload

---
+++ Task59.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int n,a, b, c, d;
+	cin>>n;
+	d=n%10;
+	n=n/10;
+	c=n%10;
+	n=n/10;
+	b=n%10;
+	n=n/10;
+	a=n;
+	if (a>b && a>c && a>d && b>c && b>d && c>d) {
+		cout << "yes";	
+	} else {
+		cout << "no";
+	}
+}
