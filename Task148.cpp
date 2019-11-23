24638496771883cae8b58d91d2e0b2b6bcc4210f
Alzhan01 <57897830+Alzhan01@users.noreply.github.com>
2019-11-23 18:53:53 +0600

    Create Task148.cpp

---
+++ Task148.cpp
@@ -0,0 +1,12 @@
+#include <iostream>                    
+int main() {
+	int x;
+	string s = "";
+	cin >> x;
+	while (x > 0) {
+		s += char(97 + (x % 10));
+		s += char((97 - (x % 10)) + 25);
+		x /= 10;
+	}
+	cout << s;
+} 
